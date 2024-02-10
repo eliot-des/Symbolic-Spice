798cea96097f9608518c1ad61bb32532f23b2526
Eliot Deschang <72158380+eliot-des@users.noreply.github.com>
2024-02-10 23:38:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct 22 14:25:17 2023
+from netlist_class import *
 
-@author: eliot
-"""
-
-from lib import create_component, display_components, create_A_matrix, create_x_vector, create_z_vector, solve_x_vector
-import sympy as sp
-
-
-sp.init_printing()
+#declaration of the netlist
+inputList  =   ['0 1 Vin 1', 
+                '1 2 R1 8',
+                '2 3 L1 4',
+                '2 0 R2 1',
+                '2 0 L2 1',
+                '3 0 C1 1']
 
 
-netlist = ['0 1 Vg', 
-           '1 2 R1',
-           '2 3 L1',
-           '3 0 C1']
+netlist = Netlist(inputList)
+
+netlist.display_components()
 
 
-solution = solve_x_vector(netlist)
+#system of the form Ax = b
+A = netlist.A
+x = netlist.x
+z = netlist.b
 
-sp.pprint(solution)
-
-
-
-#creation of the objects of the different subclasses from the netlist 
-components_list = [create_component(item) for item in netlist]
-
-
-#display the interpretation of the netlist by the algorithm
-display_components(components_list)
-
-
-#system of the form Ax = z
-A = create_A_matrix(components_list)
-x = create_x_vector(components_list)
-z = create_z_vector(components_list)
+solution =  sp.simplify(A.LUsolve(z))
 
 
 print('\n\nA matrix :\n ')
 sp.pprint(A)
 print('\n\nx vector :\n ')
 sp.pprint(x)
-print('\n\nz vector :\n ')
-sp.pprint(z)
-
-
-
-solution =  sp.simplify(A.LUsolve(z))
-
-
-
-
+print('\n\nb vector :\n ')
+sp.pprint(b)
+print('\n\nSolution :\n ')
 sp.pprint(sp.Eq(x , solution))
-
-#print Transfert function across R1
-sp.pprint(sp.simplify((solution[0]-solution[1])/solution[0]))
 
