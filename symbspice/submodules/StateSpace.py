bb4f26fb1c8080706d3896eb4f0cf28eae7c1b29
Eliot Deschang <eliot.deschang@gmail.com>
2024-08-20 22:57:37 +0200

    Add state_equations() method. Temporary suppression of y, C, D.

--- symbspice/submodules/StateSpace.py
+++ symbspice/submodules/StateSpace.py
@@ -4,12 +4,27 @@ import numpy as np
 
 
 class StateSpace:
-    def __init__(self, circuit, A, x, B, u, C, D, y):
+    def __init__(self, circuit, A, x, B, u):
         self.circuit = circuit
         self.A = A
         self.x = x
         self.B = B
         self.u = u
-        self.C = C
-        self.D = D
-        self.y = y
+
+    def state_equations(self):
+        """
+        Show the state equation of the system in the console
+
+        dx/dt = Ax + Bu
+        """
+        
+        if len(self.x) == 0:
+            return print('The circuit has no state variables...')
+        
+        x_dot = sp.Matrix([sp.Derivative(xi, 't') for xi in self.x])
+
+        if len(self.u) == 0:
+            return sp.Eq(x_dot, sp.MatMul(self.A, self.x))
+        else:
+            return sp.Eq(x_dot, sp.MatAdd(sp.MatMul(self.A, self.x) ,sp.MatMul(self.B, self.u)))
+        
