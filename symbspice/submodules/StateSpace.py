5b0e36498c6816e0ed37f0c94371c32222ca1f07
Eliot Deschang <eliot.deschang@gmail.com>
2024-08-25 18:31:32 +0200

    Add output_equations() method

--- symbspice/submodules/StateSpace.py
+++ symbspice/submodules/StateSpace.py
@@ -4,12 +4,15 @@ import numpy as np
 
 
 class StateSpace:
-    def __init__(self, circuit, A, x, B, u):
+    def __init__(self, circuit, A, x, B, u, C, y, D):
         self.circuit = circuit
         self.A = A
         self.x = x
         self.B = B
         self.u = u
+        self.C = C
+        self.y = y
+        self.D = D
 
     def state_equations(self):
         """
@@ -27,4 +30,19 @@ class StateSpace:
             return sp.Eq(x_dot, sp.MatMul(self.A, self.x))
         else:
             return sp.Eq(x_dot, sp.MatAdd(sp.MatMul(self.A, self.x) ,sp.MatMul(self.B, self.u)))
+
+    def output_equations(self):
+        """
+        Show the output equation of the system in the console
+
+        y = Cx + Du
+        """
+        
+        if len(self.y) == 0:
+            return print('The circuit has no output variables...')
+        
+        if len(self.u) == 0:
+            return sp.Eq(self.y, sp.MatMul(self.C, self.x))
+        else:
+            return sp.Eq(self.y, sp.MatAdd(sp.MatMul(self.C, self.x), sp.MatMul(self.D, self.u)))
         
